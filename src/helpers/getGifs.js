1b442ad9d3cde3dffb3b2b21664e27fdc185e427
Francisco Mulet <fmulete@gmail.com>
2022-05-02 21:04:48 -0400

    New docs modified

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=WUdeHnnBVNA74e0fknBJ9mS71yQwVUWe`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=WUdeHnnBVNA74e0fknBJ9mS71yQwVUWe`;
     const response = await fetch(url);
     const { data } = await response.json();
 
